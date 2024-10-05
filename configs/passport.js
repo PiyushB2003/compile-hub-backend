36735a152aaa316fe5a581c920294c4d88de5569
Piyush <piyushborkar95@gmail.com>
2024-10-06 00:22:01 +0530

    Callback url added

--- configs/passport.js
+++ configs/passport.js
@@ -7,7 +7,7 @@ import User from '../models/User.js';
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: "/auth/google/callback",
+    callbackURL: process.env.CALLBACK_URL,
 },
     async (accessToken, refreshToken, profile, done) => {
         try {
